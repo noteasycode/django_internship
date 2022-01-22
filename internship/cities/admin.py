b7f8ff5ba422348d5b440b35194366f1ead061b1
Denis <denis.kr.pr@gmail.com>
2022-01-22 19:58:48 +0200

    add models and config admin site 9 task

--- internship/cities/admin.py
+++ internship/cities/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from . models import Country, City
 
-# Register your models here.
+
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('text', 'pub_date', 'author')
+    search_fields = ('name',)
+    empty_value_display = '-empty-'
+
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('text', 'pub_date', 'author')
+    search_fields = ('name',)
+    list_filter = ('country', 'with_mcdonalds')
+    empty_value_display = '-empty-'
+
+
+admin.site.register(Country, CountryAdmin, City, CityAdmin)
--- internship/cities/models.py
+++ internship/cities/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class Country(models.Model):
+    name = models.CharField()
+    country_code = models.CharField(max_length=3)
+    population = models.IntegerField()
+    flag = models.ImageField(
+        'Flag',
+        upload_to='flags/',
+        blank=True
+    )
+
+    class Meta:
+        verbose_name = 'Country'
+        verbose_name_plural = 'Countries'
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    name = models.CharField()
+    description = models.TextField(blank=True, null=True)
+    country = models.ForeignKey(
+        Country,
+        on_delete=models.CASCADE,
+        related_name='cities'
+    )
+    population = models.IntegerField(blank=True, null=True)
+    slug = models.SlugField(blank=True, null=True)
+    emblem = models.ImageField(
+        'Emblem',
+        upload_to='emblems/',
+        blank=True
+    )
+    with_mcdonalds = models.BooleanField()
+
+    class Meta:
+        verbose_name = 'City'
+        verbose_name_plural = 'Cities'
+
+    def __str__(self):
+        return self.name
