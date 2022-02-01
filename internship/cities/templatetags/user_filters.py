54aee21663718d795f28b745e4a452d3c4e428fb
Denis <denis.kr.pr@gmail.com>
2022-02-01 19:34:25 +0200

    add custom filter

---
+++ internship/cities/templatetags/user_filters.py
@@ -0,0 +1,13 @@
+from django import template
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+
+@register.filter()
+def add_image(value):
+    if value:
+        result = '<img src="/media/icons/icon-yes.svg">'
+    else:
+        result = '<img src="/media/icons/icon-no.svg">'
+    return mark_safe(result)
